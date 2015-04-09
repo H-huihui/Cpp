f1cf58ce00f50d7b0407191b621f3e13cad8e10f
Masoon <han706964362@gmail.com>
2015-04-09 14:11:02 +0800

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,51 +1,47 @@
-#include<iostream.h>
-using std::string;
+#include<iostream>
+#include<string>
 
-class Stud
-{
-	private:
-    string name;
+using namespace std;
+
+class Stude{
+private:
 	int no;
-	int deg;
-	static int sum,num;
-	public:
-	Stud(string name,int no,int deg){
-		this->name=name;
-		this->no=no;
-		this->deg=deg;
-		num=num+1;
-		sum=sum+deg;
+	string name;
+	float deg;
+	static int num;
+	static int sum;
+public:
+
+	Stude(int no,string name,float deg);
+	void disp()
+	{
+		cout<<"the no is: "<<no<<endl;
+		cout<<"the name is: "<<name<<endl;
+		cout<<"the deg is: "<<deg<<endl;
+		cout<<"the sum is: "<<sum<<endl;
+		cout<<"the num is: "<<num<<endl;
 	}
-	void disp(){
-		cout<<"学生的名字是:"<<name<<endl;
-		cout<<"学生的学号是："<<no<<endl;
-		cout<<"学生的成绩是："<<deg<<endl;
-		cout<<"总学生数是："<<num<<endl;
-		cout<<"学生的总成绩是："<<sum<<endl;
+	void avg()
+	{
+		cout<<"the average number is: "<<sum/num<<endl;
 	}
-	static void avg(){
-		cout<<"学生的平均成绩是： "<<sum/num<<endl;
-		
-	}
-		
 };
-int Stud::sum=0;
-int Stud::num=0;
-int main(void)
+int Stude::sum=0;
+int Stude::num=0;
+Stude::Stude(int no,string name,float deg)
 {
-	Stud s1("韩正辉",1,90);
-	s1.disp();
-	s1.avg();
-	Stud s2("李鑫磊",2,90);
-	s2.disp();
-	Stud s3("乔硕",3,20);
-	s3.disp();
-	Stud s4("聂伟",4,99);
-	s4.disp();
-	s3.avg();
-	s2.avg();
-	s1.avg();
-	s4.avg();
-	Stud s[]={Stud("han",5,100),Stud("li",6,99),Stud("dong",7,1000),Stud("xxx",8,100)};
-	s1.disp();
+	this->no=no;
+	this->name=name;
+	this->deg=deg;
+	num++;
+	sum=sum+deg;
+}
+void main(void)
+{
+	Stude stu1(1,"han",88);
+	Stude stu[]={Stude(2,"li",99),Stude(3,"xx",77),Stude(4,"li",89)};
+	stu[1].disp();
+	cout<<endl;
+	stu1.disp();
+	stu1.avg();
 }
